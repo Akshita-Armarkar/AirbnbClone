0d226aed6d56c1828d37e1dbba6c7cbbc8ebbf2c
Akshita <akshitaarmarkar96@gmail.com>
2023-12-19 17:17:20 +0530

    added section and navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import Section from './components/Section/Section';
 
 function App() {
   return (
-    <div className="App">
+    <div className='container'>
      <Navbar/>
+     <Section/>
     </div>
   );
 }
